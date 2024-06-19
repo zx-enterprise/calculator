bec208c95e10e89dc7ff1467d4a9ab3d5564fe19
Anton Karpenko <karpolan@karpolan.com>
2024-06-19 20:00:00 +0200

    feat: state for calculator refactor: InputField

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,67 @@
+import { useState } from 'react';
 import {
   InputField,
   Keyboard,
   // Keyboard
 } from './components';
 
+type Operator = '' | '+' | '-' | '*' | '/';
+
 function App() {
+  const [value, setValue] = useState('0');
+  const [prevValue, setPrevValue] = useState('0');
+  const [operator, setOperator] = useState<Operator>('');
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+    console.log(
+      'onInputChange() - oldValue:',
+      value,
+      'newValue:',
+      event.target.value
+    );
+  };
+
+  console.log('<App/>-', prevValue, operator, value);
+
   return (
     <div>
-      <h1>Calculator</h1>
+      <h1>Calculator {value}</h1>
       <div style={{ maxWidth: '800px' }}>
-        <InputField />
+        <InputField value={value} onChange={onChange} />
+
+        <button
+          onClick={() => {
+            setPrevValue(value);
+            setValue('0');
+            setOperator('+');
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setPrevValue(value);
+            setValue('0');
+            setOperator('-');
+          }}
+        >
+          -
+        </button>
+        <button
+          onClick={() => {
+            setPrevValue(prevValue + value);
+
+            setValue('0');
+            setOperator('');
+          }}
+        >
+          =
+        </button>
+
         <Keyboard />
+
+        <InputField value={value} onChange={onChange} />
 
         <button className="button">Close</button>
       </div>
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,5 +1,30 @@
-const InputField = () => {
-  return <div>InputField is under construction</div>;
+import { FunctionComponent } from 'react';
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const InputField: FunctionComponent<Props> = ({ value, onChange }) => {
+  // const [value, setValue] = useState(propValue);
+
+  // useEffect(() => {
+  //   console.log('<InputField/> - useEffect() - value:', value);
+  // }, [value]);
+
+  // function onInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+  //   setValue(event.target.value);
+  // }
+
+  // const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setValue(event.target.value);
+  //   console.log(
+  //     'onInputChange() - oldValue:',
+  //     value,
+  //     'newValue:',
+  //     event.target.value
+  //   );
+  // };
+
+  // console.log('<InputField/> - value:', value);
+  return <input value={value} onChange={onChange} />;
 };
 
 export default InputField;
